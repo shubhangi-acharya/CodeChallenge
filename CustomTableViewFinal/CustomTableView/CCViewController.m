8d2087f89d7e6bc905179fe9c80763c84eaed535
Shubhangi Acharya <shubhangi.acharya@ecommera.co.uk>
2014-04-12 13:23:40 +0530

    CodeChallenge minor change

--- CustomTableViewFinal/CustomTableView/CCViewController.m
+++ CustomTableViewFinal/CustomTableView/CCViewController.m
@@ -12,8 +12,6 @@
 @property (nonatomic, strong) NSArray *timelineData;
 @property (nonatomic, strong) NSArray *timelineUserData;
 @property (nonatomic, strong) NSArray *timelineSourceData;
-@property (nonatomic, strong) NSArray *timelineTextData;
-
 
 @end
 
@@ -56,13 +54,14 @@
     UIRefreshControl *refreshControl = [[UIRefreshControl alloc]
                                         init];
     refreshControl.tintColor = [UIColor magentaColor];
-    [refreshControl addTarget:self action:@selector(changeSorting) forControlEvents:UIControlEventValueChanged];
+    refreshControl.attributedTitle = [[NSMutableAttributedString alloc] initWithString:@"Pull to Refresh"];
+    [refreshControl addTarget:self action:@selector(refreshTimeline) forControlEvents:UIControlEventValueChanged];
     self.refreshControl = refreshControl;
     // Calling method to fetch timeline data
     [self fetchTimeline];
 }
 
-- (void)changeSorting {
+- (void)refreshTimeline {
     [self fetchTimeline];
     [self.refreshControl endRefreshing];
 }
@@ -111,7 +110,6 @@
     NSInteger rowCount = [self tableView:[self tableView] numberOfRowsInSection:0];
     NSInteger rowIndex = indexPath.row;
     UIImage *background = nil;
-    
     if (rowIndex == 0) {
         background = [UIImage imageNamed:@"cell_top.png"];
     } else if (rowIndex == rowCount - 1) {
@@ -119,21 +117,21 @@
     } else {
         background = [UIImage imageNamed:@"cell_middle.png"];
     }
-    
+
     return background;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[NSString stringWithFormat:@"%d", indexPath.row]];
-    
+
     // Configure the cell...
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:[NSString stringWithFormat:@"%d", indexPath.row]];
     }
-    
+
     NSArray *avatarToDisplay = [NSMutableArray array];
     avatarToDisplay = [[self.timelineUserData objectAtIndex:indexPath.row] valueForKey:@"avatar_image"];
-    
+
     // Async Image loading
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSData *imgData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[avatarToDisplay valueForKey:@"url"]]];
@@ -151,29 +149,23 @@
             }
         }
     });
-    
+
     // Code for rounded corners.
     cell.imageView.contentMode =  UIViewContentModeScaleAspectFill;
     cell.imageView.layer.cornerRadius = 16;
     cell.imageView.clipsToBounds = YES;
-    
+
     // Name text
     cell.textLabel.text = [[self.timelineSourceData objectAtIndex:indexPath.row] valueForKey:@"name"];
-    
+
     // Text with dynamic size
     NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString: [[self.timelineData objectAtIndex:indexPath.row ] valueForKey:@"text"]];
-    [attributedString setAttributes:@{NSBackgroundColorAttributeName:[UIColor whiteColor]} range:NSMakeRange(0, attributedString.length)];
-    [attributedString setAttributes:@{NSForegroundColorAttributeName:[UIColor colorWithWhite:0.23 alpha:1.0]} range:NSMakeRange(0, attributedString.length)];
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle setLineBreakMode:NSLineBreakByWordWrapping];
-    [attributedString setAttributes:@{NSParagraphStyleAttributeName:paragraphStyle} range:NSMakeRange(0, attributedString.length)];
     [attributedString setAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:12]} range:NSMakeRange(0, attributedString.length)];
     cell.detailTextLabel.attributedText = attributedString;
     CGRect boundingRect = [attributedString boundingRectWithSize:CGSizeMake(cell.detailTextLabel.frame.size.width, 2000.0f) options:NSStringDrawingUsesLineFragmentOrigin context:nil];
     cell.detailTextLabel.frame = CGRectMake(cell.detailTextLabel.frame.origin.x, cell.detailTextLabel.frame.origin.y, boundingRect.size.width, boundingRect.size.height);
     cell.detailTextLabel.numberOfLines = 0;
     cell.detailTextLabel.lineBreakMode = NSLineBreakByWordWrapping;
-    
     
     // Assigning my own background image for the cell to make it nice
     UIImage *background = [self cellBackgroundForRowAtIndexPath:indexPath];
